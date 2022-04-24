7849b6d1a0d2796d7201708c6cb0cd1ec9d76165
snowstorm003 <tusharmukhopadhyay03@gmail.com>
2022-04-24 15:13:50 +0530

    my first linked list

--- Linked List/li.c
+++ Linked List/li.c
@@ -1,10 +1,49 @@
+// Program to create my firs linked list
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct st
+typedef struct st
 {
     int data;
-    struct st*next;
-}node;
+    struct st *next;
+} node;
 
+node *create_node(int value)
+{
+    node *temp;
+
+    temp = (node *)malloc(sizeof(node));
+    temp->data = value;
+    temp->next = NULL;
+
+    return temp;
+}
+
+void append(node *head, int value)
+{
+    node *temp;
+
+    temp = create_node(value);
+
+    if (head == NULL)
+        head = temp;
+
+    if (head->next == NULL)
+    {
+        head->next = temp;
+    }
+
+    if (head->next->next == NULL)
+    {
+        head->next->next = temp;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+
+    append(head, 5);
+    return 0;
+}
